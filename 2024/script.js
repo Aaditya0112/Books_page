bd165d7a29afc06e8ae9c372c7219d5edbd997b2
Aaditya0112 <rajaaditya0112@gmail.com>
2024-11-10 18:56:14 +0530

    js update

--- 2024/script.js
+++ 2024/script.js
@@ -1,37 +1,104 @@
-// const P = document.getElementById('Physics');
-// const C = document.getElementById('Chemistry');
-// const M = document.getElementById('Mathematics');
+import DATA from './data.js'
 
+const  createCards = () => {
+    const card_box = document.querySelector('.card_box')
+    DATA.forEach((item)=>{
+        const divMain = document.createElement('div')
+        const divSub1 = document.createElement('div')
+        const divSub2 = document.createElement('div')
+        const div1 = document.createElement('div')
+        const div11 = document.createElement('div')
+        const div12 = document.createElement('div')
+        const image  = document.createElement('img')
+        const h3 = document.createElement('h3')
+        const h4 = document.createElement('h4')
+        const p = document.createElement('p')
 
-function filter() {
-    var checkboxes = document.getElementsByName("check")
-    var checked = [];
+        divMain.classList.add('card')
 
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            checked.push(checkboxes[i].id);
-        }
-    }
-    var cards = document.getElementsByClassName('card');
-    for (var i = 0; i < cards.length; i++) {
-        if (checked.length === 0) {
-            cards[i].style.display = 'flex';
-        }
-        else {
-            cards[i].style.display = 'none';
-            for (var j = 0; j < checked.length; j++) {
-                if (cards[i].querySelector('.sub').innerHTML === checked[j]) {
-                    cards[i].style.display = 'flex';
+        image.src = item.img
+        image.alt = item.name
+        divMain.appendChild(image)
+
+        divSub1.classList.add('card-title')
+        h3.innerHTML = item.name
+        divSub1.appendChild(h3)
+        divMain.appendChild(divSub1)
+
+        divSub2.classList.add('card-info')
+                div1.classList.add('basic')
+                div11.classList.add('authName')  
+                h4.innerHTML = item.Author
+                div11.appendChild(h4)  
+            div1.appendChild(div11)
+                div12.classList.add('sub')
+                div12.innerHTML = item.sub
+                div1.appendChild(div12)
+                p.innerHTML = item.desc
+            divSub2.appendChild(div1)
+            divSub2.appendChild(p)
+        divMain.appendChild(divSub2)
+
+        card_box.appendChild(divMain)
+    })
+    
+}
+
+const selected = []
+const filterOptions = (id) =>{
+    const button = document.querySelector(`#${id}`);
+    button.classList.toggle('active');
+    button.classList.contains('active') ? selected.push(id) : selected.splice(selected.indexOf(id), 1) 
+    
+
+    const cards = document.querySelectorAll('.card')
+    if(selected.length === 0){
+        cards.forEach((item)=>{
+            item.style.display = 'flex'
+        })
+    }else{
+        cards.forEach((item)=>{
+            item.style.display = 'none'
+        })
+
+        //can be optimized
+        
+        selected.forEach((item)=>{
+            cards.forEach((card)=>{
+                if(card.querySelector('.sub').innerHTML === item){
+                    card.style.display = 'flex'
                 }
-            }
-        }
+            })
+        })
     }
+    
+}
+
+const FILTER = ['Physics', 'Chemistry', 'Mathematics'];
+const createOption = () => {
+    const filter = document.querySelector('.filter')
+    FILTER.forEach((item)=>{
+        const div = document.createElement('div')
+        div.classList.add('filter-option')
+        div.innerHTML = `<button class="filter-button" id = ${item}>${item}</button>`
+        filter.appendChild(div)
+    })
+    const ch = document.querySelectorAll('.filter-button')
+    ch.forEach((item)=>{
+        item.addEventListener('click', ()=>{
+            filterOptions(item.id)
+        })
+    })
 }
 
 
-ch = document.getElementsByName("check")
 
 
-ch.forEach((item) => {
-    item.addEventListener('input', filter)
-})
+
+
+
+
+
+document.body.addEventListener('load', createOption());
+
+document.body.addEventListener('load', createCards())
